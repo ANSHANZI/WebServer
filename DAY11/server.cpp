3dba21a9bc7be3f0ecabde051a6f8cd63399d3be
hu <anshanzihwy@gmail.com>
2025-02-04 14:57:24 +0800

    删除多个DAY目录中的Makefile和util.h、util.cpp文件，清理不再使用的代码

--- DAY11/server.cpp
+++
@@ -1,9 +0,0 @@
-#include "src/EventLoop.h"
-#include "src/server.h"
-
-int main() {
-    Eventloop *loop = new Eventloop();
-    Server *server = new Server(loop);
-    loop->loop();
-    return 0;
-}
--- DAY12/test/server.cpp
+++
@@ -1,9 +0,0 @@
-#include "Server.h"
-#include "EventLoop.h"
-
-int main() {
-  Eventloop *loop = new Eventloop();
-  Server *server = new Server(loop);
-  loop->loop();
-  return 0;
-}
